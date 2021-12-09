67a6b46049ca3913fc505201e7301d5770bb23e9
ACQ <89824842+ACQ06@users.noreply.github.com>
2021-12-09 17:15:52 +0800

    Add files via upload

---
+++ src/main.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+/*
+    Accept: Name, Age, Number of Passenger, Travel Type, Travel Destination.
+ */
+public class Main {
+
+    /*
+        Private, Cap of 20, With 2 pilots 4 stewardess
+        Business, Cap of 30, with 2 pilots 1 assistant pilot 4 stewardess
+        Regular, Cap of 100, with 2 pilots 2 assistant pilots 8 stewardess and 40 reserved/taken seats
+    */
+
+    public static void main(String[] args) {
+        
+    new draw();
+    new MyFrame();
+
+    }
+}
